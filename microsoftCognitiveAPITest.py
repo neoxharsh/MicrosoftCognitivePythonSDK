19cb7313455c90cab83776a1d8477609e2a8f287
Harsh <neo.harsh@gmail.com>
2016-09-06 21:28:11 +0100

    Update microsoftCognitiveAPITest.py

--- microsoftCognitiveAPITest.py
+++ microsoftCognitiveAPITest.py
@@ -1,9 +1,6 @@
 
 from microsoftcognitive import MicrosoftVision
 
-
-import pprint
-
 mv = MicrosoftVision('',MicrosoftVision.VisualService.ANALYZE)
 mv.setParameters({mv.VisualFeature.CATEGORIES})
 result  = mv.jsonRecognizeImageFromURL('https://oxfordportal.blob.core.windows.net/vision/Analysis/3.jpg')
