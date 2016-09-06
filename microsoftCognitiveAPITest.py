7c592213c6f83add332b20875ac0d4d8aa15a903
Harsh Talpada <neo.harsh@gmail.com>
2016-09-06 20:52:16 +0100

    First Beta Version

--- microsoftCognitiveAPITest.py
+++ microsoftCognitiveAPITest.py
@@ -4,7 +4,7 @@ from microsoftcognitive import MicrosoftVision
 
 import pprint
 
-mv = MicrosoftVision('f30f769d21a446fb98f0b12909841f6a',MicrosoftVision.VisualService.ANALYZE)
+mv = MicrosoftVision('',MicrosoftVision.VisualService.ANALYZE)
 mv.setParameters({mv.VisualFeature.CATEGORIES})
 result  = mv.jsonRecognizeImageFromFile('https://oxfordportal.blob.core.windows.net/vision/Analysis/3.jpg')
 
